d9d50de2a4ae45331e08eb46f5ebc1ba72bf0fec
Calvin Loncaric <marvinx03@gmail.com>
2024-06-12 16:53:14 -0700

    Upgrade commons-compress: 1.22 -> 1.26.2

--- java/commons/compress.nix
+++ java/commons/compress.nix
@@ -1,17 +1,23 @@
 {lib, fetchFromGitHub, buildJavaPackage,
- osgi, zstd-jni, brotli-dec, tukaani-xz, ow2-asm}:
+ osgi, zstd-jni, brotli-dec, tukaani-xz, ow2-asm,
+ commons-io, commons-lang, commons-codec}:
 
 buildJavaPackage rec {
   pname = "commons-compress";
-  version = "1.22";
+  version = "1.26.2";
   license = lib.licenses.asl20;
   src = fetchFromGitHub {
     owner = "apache";
     repo = pname;
-    rev = "rel/${version}";
-    hash = "sha256-L7b8ajfdNRAjPMB13+X/B5ZTZ52szrlwRpfstDlAMwI=";
+    rev = "rel/${pname}-${version}";
+    hash = "sha256-nyGvRNhut46GFRW1dvN+BSiM7sauiCM57W7TIoEgWYU=";
   };
   sourceEncoding = "iso-8859-1";
+  deps = [
+    commons-io
+    commons-lang
+    commons-codec
+  ];
   compileOnlyDeps = [
     osgi.core
     zstd-jni
