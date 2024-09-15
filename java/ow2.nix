81a5f65ca937231ec1044ba081b5a2371610f4bf
Calvin Loncaric <marvinx03@gmail.com>
2024-09-15 00:49:15 -0700

    Add a few more ow2-asm submodules

--- java/ow2.nix
+++ java/ow2.nix
@@ -8,30 +8,52 @@ src = fetchurl {
   url = "https://gitlab.ow2.org/asm/asm/-/archive/ASM_9_6/asm-ASM_9_6.tar.bz2";
   hash = "sha256-/+gHEc8rVwosZVNuCYuPyJvJLFPquBycJXVKfmNe/j4=";
 };
+srcName = "asm-ASM_9_6"; # for some reason src.name doesn't work :(
 
 in rec {
 
   asm = buildJavaPackage {
     pname = "ow2-asm";
     inherit version license src;
-    srcDir = "asm/src/main/java";
+    sourceRoot = "${srcName}/asm";
   };
 
-  asm-tree = buildJavaPackage rec {
+  asm-tree = buildJavaPackage {
     pname = "ow2-asm-tree";
     inherit version license src;
-    srcDir = "asm-tree/src/main/java";
+    sourceRoot = "${srcName}/asm-tree";
     deps = [
       asm
     ];
   };
 
-  asm-commons = buildJavaPackage rec {
+  asm-commons = buildJavaPackage {
     pname = "ow2-asm-commons";
     inherit version license src;
-    srcDir = "asm-commons/src/main/java";
+    sourceRoot = "${srcName}/asm-commons";
     deps = [
       asm
+      asm-tree
+    ];
+  };
+
+  asm-analysis = buildJavaPackage {
+    pname = "ow2-asm-analysis";
+    inherit version license src;
+    sourceRoot = "${srcName}/asm-analysis";
+    deps = [
+      asm
+      asm-tree
+    ];
+  };
+
+  asm-util = buildJavaPackage {
+    pname = "ow2-asm-util";
+    inherit version license src;
+    sourceRoot = "${srcName}/asm-util";
+    deps = [
+      asm
+      asm-analysis
       asm-tree
     ];
   };
