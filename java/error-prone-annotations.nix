6b853f9cbc931dd84c922dc3c14a72d87a695294
Calvin Loncaric <marvinx03@gmail.com>
2024-10-17 22:16:24 -0700

    Upgrade error-prone-annotations: 2.23.0 -> 2.33.0

--- java/error-prone-annotations.nix
+++ java/error-prone-annotations.nix
@@ -2,13 +2,13 @@
 
 buildJavaPackage rec {
   pname = "error-prone-annotations";
-  version = "2.23.0";
+  version = "2.33.0";
   license = lib.licenses.asl20;
   src = fetchFromGitHub {
     owner = "google";
     repo = "error-prone";
     rev = "v${version}";
-    hash = "sha256-nCIeGSIEwwHuS2T86Ee+PhfMETU/H016lLYSmGE3fn0=";
+    hash = "sha256-uDh1WIgap7jQl6TrTUsnjxHMtWpXKUAbsCE0xj3jP8Y=";
   };
-  srcDir = "annotations/src/main/java";
+  sourceRoot = "${src.name}/annotations";
 }
