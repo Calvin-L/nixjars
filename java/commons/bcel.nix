3560027c5280f688e8b58f13cc96fdcd982cbec5
Calvin Loncaric <marvinx03@gmail.com>
2024-10-17 20:52:58 -0700

    Upgrade commons-bcel: 6.7.0 -> 6.10.0

--- java/commons/bcel.nix
+++ java/commons/bcel.nix
@@ -2,13 +2,13 @@
 
 buildJavaPackage rec {
   pname = "commons-bcel";
-  version = "6.7.0";
+  version = "6.10.0";
   license = lib.licenses.asl20;
   src = fetchFromGitHub {
     owner = "apache";
     repo = "commons-bcel";
     rev = "rel/${pname}-${version}";
-    hash = "sha256-/K3zmPWxzxVK/2AAj6sZQAvlTHA4eMUvFeGf7ZXi5+4=";
+    hash = "sha256-2dCbSK6SkBoGAHnbxrqZIHV+ogjelnob7jXrutPMRM0=";
   };
   srcDir = "src/main";
   deps = [
