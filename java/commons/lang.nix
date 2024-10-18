f10cbbc2a3eac6df86d938ee05876da559cfd604
Calvin Loncaric <marvinx03@gmail.com>
2024-10-17 21:02:20 -0700

    Upgrade commons-lang: 3.14.0 -> 3.17.0

--- java/commons/lang.nix
+++ java/commons/lang.nix
@@ -2,13 +2,13 @@
 
 buildJavaPackage rec {
   pname = "commons-lang";
-  version = "3.14.0";
+  version = "3.17.0";
   license = lib.licenses.asl20;
   src = fetchFromGitHub {
     owner = "apache";
     repo = "commons-lang";
     rev = "rel/${pname}-${version}";
-    hash = "sha256-9q8yHmZ5KR5mqyXCCoNxXEmB8oJSq1u1BokbiNWTzis=";
+    hash = "sha256-JTtRG3OTjqNA5ULWm4yEe2aHF9CH9TqfFUbYs8kX334=";
   };
   srcDir = "src/main";
 }
