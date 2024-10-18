c17765b0f71d50ed797ddee342928fe33f973ac5
Calvin Loncaric <marvinx03@gmail.com>
2024-10-17 21:08:01 -0700

    Upgrade guava: 33.2.1 -> 33.3.1

--- java/guava.nix
+++ java/guava.nix
@@ -5,13 +5,13 @@
 
 let
 
-  guava-version = "33.2.1";
+  guava-version = "33.3.1";
   guava-license = lib.licenses.asl20;
   guava-src = fetchFromGitHub {
     owner = "google";
     repo = "guava";
     rev = "v${guava-version}";
-    hash = "sha256-zHoHRuPj1mFUbBR2P31qI2t17twXLRGnGLniZeD0NsQ=";
+    hash = "sha256-SnZ7+lft8r/VXzaY+jGb4SWhJyeEm4uW+s6V6r8qI6M=";
   };
 
 in
