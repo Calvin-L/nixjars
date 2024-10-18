bef947925d7f060b205f77a45cf2d292488c3796
Calvin Loncaric <marvinx03@gmail.com>
2024-10-17 21:10:59 -0700

    Upgrade jna: 5.14.0 -> 5.15.0

--- java/jna.nix
+++ java/jna.nix
@@ -2,13 +2,13 @@
 
 buildJavaPackage rec {
   pname = "jna";
-  version = "5.14.0";
+  version = "5.15.0";
   license = lib.licenses.asl20;
   src = fetchFromGitHub {
     owner = "java-native-access";
     repo = pname;
     rev = version;
-    hash = "sha256-a5l9khKLWfvTHv53utfbw344/UNQOnIU93+wZNQ0ji4=";
+    hash = "sha256-PadOJtoH+guPBQ/j6nIBp7BokNz23OQhaYpcFl/wbpQ=";
   };
   srcDir = "src";
   # checkPhase = testWithJUnit4 { testSrcDirs = ["test"]; testDeps = [hamcrest]; }; # package org.reflections does not exist
