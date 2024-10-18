ddbb52d5692637bb988631b2b231f9d891d226a8
Calvin Loncaric <marvinx03@gmail.com>
2024-10-17 21:15:51 -0700

    Upgrade tukaani-xz: 1.9 -> 1.10

--- java/tukaani-xz.nix
+++ java/tukaani-xz.nix
@@ -2,13 +2,13 @@
 
 buildJavaPackage rec {
   pname = "tukaani-xz";
-  version = "1.9";
+  version = "1.10";
   license = lib.licenses.publicDomain; # https://tukaani.org/xz/java.html, 2023/10/1
   src = fetchFromGitHub {
     owner = "tukaani-project";
     repo = "xz-java";
     rev = "v${version}";
-    hash = "sha256-W3CtViPiyMbDIAPlu5zbUdvhMkZLVxZzB9niT49jNbE=";
+    hash = "sha256-vW0C4QrRY0VBIpbWKEGVGfgsxg7MyFU++VLnbbKUb+c=";
   };
   srcDir = "src";
 }
