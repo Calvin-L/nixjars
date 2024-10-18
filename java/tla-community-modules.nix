a1e951fb3a8c06208a50b6338394388df7304dd1
Calvin Loncaric <marvinx03@gmail.com>
2024-10-17 21:15:30 -0700

    Upgrade tla-community-modules: 202405171516 -> 202409181925

--- java/tla-community-modules.nix
+++ java/tla-community-modules.nix
@@ -4,12 +4,12 @@
 buildJavaPackage rec {
   pname = "tla-community-modules";
   license = lib.licenses.mit;
-  version = "202405171516";
+  version = "202409181925";
   src = fetchFromGitHub {
     owner = "tlaplus";
     repo = "CommunityModules";
     rev = version;
-    hash = "sha256-xdutDP1LYJYswIBgB8RjhEWHUREI96e+Xba5m0Ulxmg=";
+    hash = "sha256-bHZYUdy7dXlJ9iwGVK4aO4SxOgwNpX8R2ahOZN5Sxrw=";
   };
   srcDir = "modules";
   resourceDir = "modules";
