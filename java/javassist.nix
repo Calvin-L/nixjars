559975b152e81650e05d92bae46c30074ef69109
Calvin Loncaric <marvinx03@gmail.com>
2024-10-17 21:09:31 -0700

    Upgrade javassist: 3.27.0-GA -> 3.30.2

--- java/javassist.nix
+++ java/javassist.nix
@@ -2,13 +2,13 @@
 
 buildJavaPackage rec {
   pname = "javassist";
-  version = "3.27.0-GA";
+  version = "3.30.2";
   license = lib.licenses.mit;
   src = fetchFromGitHub {
     owner = "jboss-javassist";
     repo = "javassist";
-    rev = "rel_3_27_0_ga";
-    sha256 = "1m1vhppn3va9rk66g6lf877mx7k66fvcn4rj64s6py42q0hvnlxy";
+    rev = "rel_3_30_2_ga";
+    hash = "sha256-Y0tk+IP/zXSOXyfXZtq4xnl2dJq3DEP2RF4yGIvNQ0A=";
   };
   srcDir = "src/main/javassist";
 }
