a0c907ec6e17c20555de91cb2c7b9767dc04fba1
Calvin Loncaric <marvinx03@gmail.com>
2024-10-17 22:18:53 -0700

    Upgrade spotbugs-annotations: 4.8.3 -> 4.8.6

--- java/spotbugs-annotations.nix
+++ java/spotbugs-annotations.nix
@@ -2,13 +2,13 @@
 
 buildJavaPackage rec {
   pname = "spotbugs-annotations";
-  version = "4.8.3";
+  version = "4.8.6";
   license = lib.licenses.lgpl21;
   src = fetchFromGitHub {
     owner = "spotbugs";
     repo = "spotbugs";
     rev = version;
-    hash = "sha256-L61Fs+pbVdxP4pkgKfHILmhQf/jd35Mb66U9o2qvXno=";
+    hash = "sha256-1i2FhedUvPRIVrJf4sF/TPaRFliOKaVCT83d8GQFEFA=";
   };
   sourceRoot = "${src.name}/${pname}";
   deps = [
