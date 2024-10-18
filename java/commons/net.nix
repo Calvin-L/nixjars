fe245c302e0c350cc7b20cf06a366b1e0e6fc147
Calvin Loncaric <marvinx03@gmail.com>
2024-10-17 21:03:15 -0700

    Upgrade commons-net: 3.10.0 -> 3.11.1

--- java/commons/net.nix
+++ java/commons/net.nix
@@ -2,14 +2,12 @@
 
 buildJavaPackage rec {
   pname = "commons-net";
-  version = "3.10.0";
+  version = "3.11.1";
   license = lib.licenses.asl20;
   src = fetchFromGitHub {
     owner = "apache";
     repo = "commons-net";
     rev = "rel/${pname}-${version}";
-    hash = "sha256-4kSxWuq5mDC4Iy1RTyGlxmf/Fg+u8e77/eiFJTKsQ2U=";
+    hash = "sha256-TzzYKIWDD+rtugpJIf6mmV2Zgm0heBcJgUkdircIeRE=";
   };
-  deps = [
-  ];
 }
