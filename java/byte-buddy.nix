7e49c30253857edb62c34b27fe217462665df20e
Calvin Loncaric <marvinx03@gmail.com>
2024-10-17 21:56:57 -0700

    Upgrade byte-buddy: 1.14.12 -> 1.15.5

--- java/byte-buddy.nix
+++ java/byte-buddy.nix
@@ -3,13 +3,13 @@
 
 buildJavaPackage rec {
   pname = "byte-buddy";
-  version = "1.14.12";
+  version = "1.15.5";
   license = lib.licenses.asl20;
   src = fetchFromGitHub {
     owner = "raphw";
     repo = pname;
     rev = "${pname}-${version}";
-    hash = "sha256-SL6tEBgb8nMYd7yUu8Vf5MiTlqjBFmho3/SAuT4qxrw=";
+    hash = "sha256-wHdznDFto4sd8RGXJjCdef05YsXIXKtBCjZvjq5cFQs=";
   };
   sourceRoot = "${src.name}/byte-buddy-dep";
   deps = [
