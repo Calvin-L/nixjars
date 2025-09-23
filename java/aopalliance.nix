8f4690128ddebd3d35b8b20894d8e26e8a13f4a3
Calvin Loncaric <marvinx03@gmail.com>
2025-09-23 16:22:01 -0700

    Fix CVS downloads of ancient packages (again)

--- java/aopalliance.nix
+++ java/aopalliance.nix
@@ -1,4 +1,4 @@
-{lib, buildJavaPackage, fetchcvs}:
+{lib, buildJavaPackage, fetchurl, unzip, cvs}:
 
 buildJavaPackage rec {
   pname = "aopalliance";
@@ -11,10 +11,30 @@ buildJavaPackage rec {
   #   sparseCheckout = ["aopalliance"];
   #   hash = "sha256-l8cAsn61o46HJWDUWrCZKhy0HrSWpw9pyOrk7sPIfxc=";
   # };
-  src = fetchcvs {
-    cvsRoot = ":pserver:anonymous@aopalliance.cvs.sourceforge.net:/cvsroot/aopalliance";
-    module = "aopalliance";
-    sha256 = "75Zr5f9chwVN39s6VCQgrc9HmToQJxRg4DrEZbWwbrc=";
+  # src = fetchcvs {
+  #   cvsRoot = ":pserver:anonymous@aopalliance.cvs.sourceforge.net:/cvsroot/aopalliance";
+  #   module = "aopalliance";
+  #   sha256 = "75Zr5f9chwVN39s6VCQgrc9HmToQJxRg4DrEZbWwbrc=";
+  # };
+
+  # With SourceForge CVS offline, this is a bit awkward.  They offer a zip of
+  # the CVS repo, not the source, so we need to use CVS to get a working source
+  # tree.
+  src = fetchurl {
+    url = "https://sourceforge.net/code-snapshots/cvs/a/ao/aopalliance.zip";
+    hash = "sha256-+zEwT3M/bqpGBTtBiWVPawUHL7D30QFje4jCg8X/WgE=";
   };
+  nativeBuildInputs = [
+    unzip
+    cvs
+  ];
+  sourceRoot = ".";
+  postUnpack = ''
+    mkdir checkout
+    cd checkout
+    cvs -d "$(pwd)/../aopalliance" co aopalliance
+    cd aopalliance
+  '';
+
   srcDir = "src/main";
 }
--- java/isorelax.nix
+++ java/isorelax.nix
@@ -1,4 +1,4 @@
-{lib, buildJavaPackage, fetchcvs}:
+{lib, buildJavaPackage, fetchurl, unzip, cvs}:
 
 buildJavaPackage {
   pname = "isorelax";
@@ -10,12 +10,27 @@ buildJavaPackage {
   #   rev = "75e2882eaf4133bb7efc762ff9f37548cb19b836";
   #   hash = "sha256-5O5uiywjYNkwx5HDng/DWAMMnzUFKMKvtfLwFC2jo00=";
   # };
-  src = fetchcvs {
-    cvsRoot = ":pserver:anonymous@iso-relax.cvs.sourceforge.net:/cvsroot/iso-relax";
-    module = "src";
-    sha256 = "sha256-Sdmao7juS+Yb4k+xN1qDXjuGz3VIXBRLuMg6VkxJeEQ=";
+
+  # src = fetchcvs {
+  #   cvsRoot = ":pserver:anonymous@iso-relax.cvs.sourceforge.net:/cvsroot/iso-relax";
+  #   module = "src";
+  #   sha256 = "sha256-Sdmao7juS+Yb4k+xN1qDXjuGz3VIXBRLuMg6VkxJeEQ=";
+  # };
+  src = fetchurl {
+    url = "https://sourceforge.net/code-snapshots/cvs/i/is/iso-relax.zip";
+    hash = "sha256-zFAmRHL+Hu3HRvIDM/z1r1LjVsw7qhPL6tvkAyUk+n4=";
   };
-  srcDir = ".";
+  nativeBuildInputs = [
+    unzip
+    cvs
+  ];
+  sourceRoot = ".";
+  postUnpack = ''
+    mkdir checkout
+    cd checkout
+    cvs -d "$(pwd)/../iso-relax" co src
+  '';
+  srcDir = "src";
 
   # # break a cyclic dependency...
   # # anyways, who even uses the ant rules from this ancient project???
@@ -38,6 +53,6 @@ buildJavaPackage {
   # public boolean isValidating()'
   # '';
   patchPhase = ''
-    rm -r org/iso_relax/ant/
+    rm -r src/org/iso_relax/ant/
   '';
 }
